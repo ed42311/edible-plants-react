823d8aa7948fbd027d0411e7062c462d7f3dd458
contextCue <14878694+ed42311@users.noreply.github.com>
2020-11-10 16:59:23 -0700

    clean overwrite

--- packages/api/dist/config/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    dbUri: 'mongodb://localhost:27017/plants',
-    jwtSecret: 'a secret phrase!!',
-};
-//# sourceMappingURL=index.js.map
